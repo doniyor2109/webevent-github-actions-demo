3d66d2f26bc02c8f54930e32e2b43f30127a4cbe
Doniyor Aliyev <doniyor2109@gmail.com>
2022-05-04 14:19:02 +0500

    Remind todos (#2)

--- api/todo.ts
+++ api/todo.ts
@@ -1,42 +1,26 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Octokit } from '@octokit/rest';
+import { TodoService } from '../services/TodoService';
 
-const octokit = new Octokit({
-  auth: process.env.GHP_TOKEN,
-});
-
-const GIST_FILE_NAME = 'webevent-github-actions-demo__todo_list.json';
-
-async function saveArtifact(request: VercelRequest, response: VercelResponse) {
+async function postGist(request: VercelRequest, response: VercelResponse) {
   if (!request.body) {
     return response.status(401).end('Empty body');
   }
-
-  await octokit.rest.gists.update({
-    gist_id: process.env.GH_GIST_ID,
-    files: { [GIST_FILE_NAME]: { content: JSON.stringify(request.body) } },
-  });
-
+  await new TodoService().save(request.body);
   response.status(200).end();
 }
 
-async function getArtifact(request: VercelRequest, response: VercelResponse) {
-  const { data } = await octokit.rest.gists.get({
-    gist_id: process.env.GH_GIST_ID,
-  });
-  const { content } = data.files[GIST_FILE_NAME];
-
-  console.log(content);
-  response.status(200).json(JSON.parse(content));
+async function getGist(request: VercelRequest, response: VercelResponse) {
+  const content = await new TodoService().get();
+  response.status(200).json(content);
 }
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   switch (request.method) {
     case 'POST':
-      await saveArtifact(request, response);
+      await postGist(request, response);
       break;
     case 'GET':
-      await getArtifact(request, response);
+      await getGist(request, response);
       break;
     case 'OPTIONS':
       return response.status(200).end();
---
+++ scripts/remind_todos.ts
@@ -0,0 +1,30 @@
+import { Telegram } from 'telegraf';
+import { TodoService } from '../services/TodoService';
+
+async function main() {
+  const chatId = 4097867;
+  const telegram = new Telegram(process.env.TELEGRAM_BOT_TOKEN);
+
+  const todos = await new TodoService().get();
+  const uncompletedTodos = todos.filter((todo) => !todo.completed);
+
+  if (uncompletedTodos.length) {
+    await telegram.sendMessage(
+      chatId,
+      `You have ${uncompletedTodos.length} uncompleted todo items.
+Try finishing them by visiting https://webevent-github-actions.vercel.app/`
+    );
+  } else {
+    await telegram.sendMessage(
+      chatId,
+      'Yay, you have completed all todo items'
+    );
+  }
+
+  console.log('Todos reminded successfully');
+}
+
+main().catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
